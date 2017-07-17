1234e70ed2b56ae2e395ae6f43c7180ad9ba305e
Ehshan Veerabangsa <ehshan@msn.com>
2017-07-17 17:53:20 +0100

    initial commit with input & output training data

---
+++ Simple-Neural-Network.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+# values for nodes
+T, F = 0, 1.
+bias = 1.
+
+# Training Set of 4 inputs
+training_in = np.array([
+    [T, T, bias],
+    [T, F, bias],
+    [F, T, bias],
+    [F, F, bias],
+])
+
+# Output patterns for an AND input set
+training_out = np.array([
+    [T],
+    [F],
+    [F],
+    [F],
+])
+
