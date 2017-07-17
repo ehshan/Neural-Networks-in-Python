fa7a864e96050bca2c0964f67cf6faae4596de12
Ehshan Veerabangsa <ehshan@msn.com>
2017-07-17 20:18:37 +0100

    added function to train network + print outputs

--- Simple-Neural-Network.py
+++ Simple-Neural-Network.py
@@ -31,3 +31,29 @@ def sigmoid(x):
 
 def gradient(x):
     return x * (1 - x)
+
+
+# 1000 epochs to train on
+for epoch in range(10000):
+    # Forward propagation - X axis is weight Y axis is error
+    # Minimization (error) problem
+    input_layer = training_in
+
+    # Applied sigmoid function to product of input and weights
+    output_layer = sigmoid(np.dot(input_layer, weights))
+    # print("Hidden: ", hidden_layer, "\n")  # values in hidden layer
+
+    # Error is the training out - output
+    error = training_out - output_layer
+    # print("Raw Error: ", "\n", error, "\n")  # the raw error
+
+    # Computes the mean of error across dimensions of array
+    print("epoch: ", epoch, "Error: ", str(np.mean(np.abs(error))), "\n")
+
+    # Product of the error the slope at (X,Y)
+    error_gradient = error * gradient(output_layer)
+
+    # Update the weights
+    weights += np.dot(input_layer.T, error_gradient)
+
+print("Final Output:", "\n", output_layer)
