a559c4bea2a32ac6c611184d06dc19f235ddc5ee
Ehshan Veerabangsa <ehshan@msn.com>
2017-07-18 21:08:31 +0100

    added function to train network + print outputs

--- 3-Layer-Neural-Network.py
+++ 3-Layer-Neural-Network.py
@@ -33,3 +33,35 @@ def sigmoid(x):
 
 def gradient(x):
     return x * (1 - x)
+
+# 10000 epochs
+for epoch in range(10000):
+    # Input layer
+    input_layer = training_in
+
+    # First hidden layer
+    hidden_layer_1 = sigmoid(np.dot(input_layer, input_weights))
+
+    # Second hidden layer
+    hidden_layer_2 = sigmoid(np.dot(hidden_layer_1, hidden_weights))
+
+    # Calculate the error
+    layer_2_error = training_out - hidden_layer_2
+
+    # Computes the mean of elements across dimensions of array
+    print("epoch: ", epoch, "Error: ", str(np.mean(np.abs(layer_2_error ))), "\n")
+
+    # The plot of the weight(x) value against the error(y)
+    layer_2_error_gradient = layer_2_error * gradient(hidden_layer_2)
+
+    # Calculate the error in the first error
+    layer_1_error = layer_2_error_gradient.dot(hidden_weights.T)
+
+    # Compute the error gradient in the first layer
+    layer_1_error_gradient = layer_1_error * gradient(hidden_layer_1)
+
+    # Update the weights (= x = x + gradient) for both layers
+    hidden_weights += np.dot(hidden_layer_1.T, layer_2_error_gradient)
+    input_weights += np.dot(input_layer.T, layer_1_error_gradient)
+
+print("Final Output:", "\n", hidden_layer_2)
